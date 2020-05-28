857feb36308099528cf96ae44b80e171fc4d67f8
AANtoso <adjoaan@gmail.com>
2020-05-28 11:18:56 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,30 +5,46 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
-  constructor() {
-    super()
-  }
+    constructor() {
+        super()
+        this.state = {
+            selectedColor: '#FFF'
+        }
+    }
 
-  genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
-  )
+    // this method updates selectedColor with whatever is passed into it as an argument
+    setSelectedColor = (newColor) => {
+        this.setState({
+            selectedColor: newColor
+        })
+    }
 
-  genMatrix = () => (
-    this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
-  )
 
-  render() {
-    return (
-      <div id="app">
-        <ColorSelector />
-        <div id="matrix">
-          {this.genMatrix()}
-        </div>
-      </div>
-    )
-  }
-}
+    genRow = (vals) => (
+            vals.map((val, idx) => < Cell key = { idx }
+                color = { val }
+                selectedColor = { this.state.selectedColor }
+                />)
+                // Cell, on the other hand, only needs to know the currently selected color, not change it
+            )
 
-Matrix.defaultProps = {
-  values: learnSymbol
-}
+            genMatrix = () => (
+                this.props.values.map((rowVals, idx) => < div key = { idx }
+                    className = "row" > { this.genRow(rowVals) } < /div>)
+                )
+
+                render() {
+                    return ( <
+                        div id = "app" > { /* ColorSelector is the interface where users will be able to select a color, so it will need access to setSelectedColor. We can pass the needed function down as a prop */ } <
+                        ColorSelector setSelectedColor = { this.setSelectedColor }
+                        /> <
+                        div id = "matrix" > { this.genMatrix() } <
+                        /div> <
+                        /div>
+                    )
+                }
+            }
+
+            Matrix.defaultProps = {
+                values: learnSymbol
+            }
